805ac5abad59253b04771347c42edf96f487ca10
ysquareimperial <ysquare.theimperial@gmail.com>
2023-08-21 19:58:09 +0100

    save

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from "react";
+import "./App.css";
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Button variant="contained" style={{ textTransform: "none" }}>
+        Contained
+      </Button>
+    </>
+  );
+}
+
+export default App;
